75354f19d92953d142312aa9c39ea6e9dd34f3a7
gavand-sagar <gavand.sagar.s@gmail.com>
2024-11-11 06:55:00 +0530

    port changesd

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use((err, req, res, next) => {
   });
   
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
